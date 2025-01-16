d59a3831021abcb4041d3fe53f051f722fa74400
Vadimka180880 <vadim.te180880@gmail.com>
2025-01-15 19:15:19 -0500

    Add missing folders and files

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,14 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  base: '/goit-advancedjs-hw-03/', 
-  root: 'src', 
+  base: '/goit-advancedjs-hw-03/', // Для GitHub Pages
+  root: '.', // Корінь проєкту (залишаємо index.html у корені)
   build: {
-    outDir: '../dist', 
-    emptyOutDir: true, 
+    outDir: 'dist', // Папка для зібраного проєкту
+    emptyOutDir: true, // Очищення dist перед новою збіркою
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'src/index.html'), 
-        timer: path.resolve(__dirname, 'src/1-timer.html'), 
-        snackbar: path.resolve(__dirname, 'src/2-snackbar.html'), 
+        main: path.resolve(__dirname, 'index.html'), // Головна сторінка
       },
     },
   },
